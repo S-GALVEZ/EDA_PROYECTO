c736e09f623e9d76a8c6a5e9c0cc8790f048cfe6
Gianfranco <20236441@aloe.ulima.edu.pe>
2025-07-06 16:48:17 -0500

    Update PROYECTO_EDA.java

--- PROYECTO_EDA/PROYECTO_EDA/src/main/java/com/mycompany/proyecto_eda/PROYECTO_EDA.java
+++ PROYECTO_EDA/PROYECTO_EDA/src/main/java/com/mycompany/proyecto_eda/PROYECTO_EDA.java
@@ -1,15 +1,22 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.proyecto_eda;
 
+package com.mycompany.proyecto_eda1;
 import VISTA.LoginPanel;
+import CLASES.GestionExpediente;
+import javax.swing.SwingUtilities;
 
-public class PROYECTO_EDA {
+/**
+ *
+ * @author Gianfranco
+ */
+public class PROYECTO_EDA1 {
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new LoginPanel().setVisible(true); // Inicia primero con el login
+    public static void main(String[] args){
+          SwingUtilities.invokeLater(() -> {
+          GestionExpediente gestionExp = new GestionExpediente();
+          new LoginPanel(gestionExp).setVisible(true); 
         });
     }
 }
